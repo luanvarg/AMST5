e0ce7479a02f04fdc68ebc41a022dadb612f0019
Javier Arce <jasearce@espol.edu.ec>
2019-10-31 11:56:47 -0500

    metodos del menu flotante

--- app/src/main/java/com/example/amst5/MainActivity.java
+++ app/src/main/java/com/example/amst5/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.amst5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creacion de botones flotantes
+        FloatingActionButton fab1 = findViewById(R.id.youtube);
+        FloatingActionButton fab2 = findViewById(R.id.calendario);
+        FloatingActionButton fab3 = findViewById(R.id.mapa);
+        FloatingActionButton fab4 = findViewById(R.id.grafico);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
